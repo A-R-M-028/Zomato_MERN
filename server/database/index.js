581c0347f476af4b10f8050404184d71074a59e7
ARM <ashikurrahamanmolla028@gmail.com>
2023-08-02 16:30:14 +0530

    Initial Setup and Schemas added

---
+++ server/database/index.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+// Key: Datatype
+const UserSchema = new mongoose.Schema({
+    fullname: {type: String, requeired: true},
+    email: {type: String, required: true},
+    password: {type: String}, // Requered -> In auth part
+    address: [{detail: {type: String}, for: {type: String}}],
+    phoneNumbber: [{type: Number}]
+})
+
+export const UserModel = mongoose.model('Users', UserSchema)
